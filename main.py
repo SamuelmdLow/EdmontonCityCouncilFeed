171182fdd112acd4e48ac0d00202df5030e65d5f
tubbdoose <samuel.m.d.low@gmail.com>
2022-07-11 03:02:41 -0600

    Made parsing more reliable

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ def getHtml(url):
     mybytes = fp.read()
     html = mybytes.decode("utf8")
     fp.close()
-    while "  " in html:
-        html = html.replace('  ', '')
-
-    html = html.replace('\n', '')
 
     return html
 
@@ -26,29 +22,36 @@ def getElemValue(start, end, html):
     for char in range(len(html)):
         if hasStart == False:
             if html[char:char+startLen] == start:
-
                 hasStart = True
                 opened = 0
                 elemStart = char + startLen
         else:
+
             if html[char:char + endLen] == end and opened == 0:
                 #print("element found!")
                 elems.append(html[elemStart:char])
                 hasStart = False
-            if html[char] == '<':
+            elif html[char] == '<':
                 if html[char:char+2] == '</':
-                    #print("closed")
                     opened -= 1
-                else:
-                    #print("opened")
+                    #print(opened)
+                elif html[char:char+4] != '<br>':
                     opened += 1
+                    #print(opened)
+                #print(html[char:char + 20] + " " + str(opened))
 
     return elems
 
 def checkMotion(elems):
+    terms = ["be read a first time", "be read a second time", "be considered for third reading", "be read a third time", "That the minutes from the following meetings be approved"]
     important = []
     for elem in elems:
-        if '<strong>' in elem:
+        good = True
+        for term in terms:
+            if term in elem:
+                good = False
+                break
+        if good == True:
             important.append(elem)
 
     return important
@@ -76,55 +79,75 @@ def cleanText(text):
         return text[text.index(">")+1:len(text)]
     return text
 
+def splitVoters(votes):
+    votes = votes.replace(" and ", " ")
+    return votes.split(", ")
+
+def outputVoters(votes):
+    output = ""
+    for voter in votes:
+        output = output + ", " + voter
+
+    return output[2:]
+
 class motion():
     def __init__(self):
-        self.name = ""
         self.movedBy = ""
         self.secondedBy = ""
         self.desc = ""
-        self.inFavour = ""
-        self.opposed = ""
-        self.carried = False
+        self.inFavour = []
+        self.opposed = []
+        self.status = ""
 
     def createMotion(self, info):
 
         self.movedBy = cleanText(info[2][1])
         self.secondedBy = cleanText(info[3][1])
-        self.name = cleanText(penetrate(info[4][0]))
-        self.desc = cleanText(info[4][1][0])
+
+
+        for text in info[4]:
+            self.desc = self.desc + " " + cleanText(penetrate(text))
+
+        self.desc = self.desc[1:]
 
         if len(info[4]) == 3:
-            self.desc = self.desc + " " + cleanText(info[4][2][0])
+            self.desc = self.desc + " " + cleanText(penetrate(info[4][2][0]))
 
-        self.inFavour = cleanText(info[5][0][1])
-        self.opposed = cleanText(info[5][1][1])
+        if len(info[5][0]) > 1:
+            self.inFavour = splitVoters(cleanText(info[5][0][1]))
+
+        if len(info[5][1]) > 1:
+            self.opposed = splitVoters(cleanText(info[5][1][1]))
+
         if "carried" in cleanText(info[6]).lower():
-            self.carried = True
+            self.status = "Carried"
+        elif "defeated" in cleanText(info[6]).lower():
+            self.status = "Defeated"
         else:
-            self.carried = False
+            self.status = "Not Voted"
 
     def output(self):
-        print("Motion: " + self.name)
         print("Moved by: " + self.movedBy)
         print("Seconded By: " + self.secondedBy)
         print("Description: " + self.desc)
-        print("In Favour: " + self.inFavour)
-        print("Opposed: " + self.opposed)
-        if self.carried:
-            print("Carried")
-        else:
-            print("Defeated")
+
+        print("In Favour: " + outputVoters(self.inFavour))
+        print("Opposed: " + outputVoters(self.opposed))
+        print(self.status)
 
 if __name__ == "__main__":
-
-    html = getHtml('https://pub-edmonton.escribemeetings.com/Meeting.aspx?Agenda=PostMinutes&Id=90d0b4a6-47d2-47f1-a735-9076e5a89c0f&Item=19&Tab=attachments&lang=English')
+    html = getHtml('https://pub-edmonton.escribemeetings.com/Meeting.aspx?Id=af273061-b1c9-4143-a0c5-a56b48d415a4&Agenda=PostMinutes&lang=English')
 
     file = open(filename, "w")
     file.write(html)
     file.close()
 
-    elems = getElemValue("<LI class='AgendaItemMotion' >", "</LI>", html)
+    while "  " in html:
+        html = html.replace('  ', '')
 
+    html = html.replace('\n', '')
+
+    elems = getElemValue("<LI class='AgendaItemMotion' >", "</LI>", html)
     elems = checkMotion(elems)
 
     motions = []
